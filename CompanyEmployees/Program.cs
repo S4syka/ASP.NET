0e01f721cda16408ea4684a0b692fba119ed1417
Eke_bichi <ereklekhomasuridze@gmail.com>
2023-10-19 11:39:09 +0400

    Added cors configuration

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -1,8 +1,12 @@
+using CompanyEmployees.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.ConfigureCors();
 
 var app = builder.Build();
 
