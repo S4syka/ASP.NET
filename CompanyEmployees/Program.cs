0e88c835fcf8a6e22021f5d11cc96418daf6e347
Eke_bichi <ereklekhomasuridze@gmail.com>
2023-10-19 13:19:14 +0400

    Refactored Program.cs

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -1,23 +1,29 @@
+using Microsoft.AspNetCore.HttpOverrides;
 using CompanyEmployees.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-
 builder.Services.ConfigureCors();
-
 builder.Services.ConfigureIISIntegration();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseHsts();
+}
 app.UseHttpsRedirection();
-
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+app.UseCors("CorsPolicy");
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
