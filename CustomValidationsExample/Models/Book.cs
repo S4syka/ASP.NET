1afabfb3fee3490596da424e6185df611b1e2f26
erekle khomasuridze <ereklekhomasuridze@gmail.com>
2024-07-06 21:49:37 +0400

    Implemented custom validations with ValidationAttribute and IValidatableObject

---
+++ CustomValidationsExample/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using Models.ValidationAttributes;
+
+namespace Models;
+
+public class Book : IValidatableObject
+{
+    public string? Name { get; set; }
+
+    [BookGenre(BookGenre.Science)]
+    public BookGenre Genre { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        var errorMessage = $"The genre of the book must be {BookGenre.Science}";
+        if (!Genre.Equals(BookGenre.Science.ToString()))
+            yield return new ValidationResult(errorMessage, new[] {nameof(Genre) });
+    }
+}
---
+++ CustomValidationsExample/Models/ValidationAttributes/BookGenreAttribute.cs
@@ -0,0 +1,26 @@
+﻿using Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.ValidationAttributes;
+
+public class BookGenreAttribute(BookGenre genre) : ValidationAttribute
+{
+    public BookGenre Genre { get; init; } = genre;
+    public string Error => $"The genre of the book must be {Genre}";
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        var book = (Book)validationContext.ObjectInstance;
+        if(book.Genre != Genre)
+        {
+            return new ValidationResult(Error);
+        }
+
+        return ValidationResult.Success;
+    }
+}
