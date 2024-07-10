0d3d96503d09bdfdf831a44a552e61981ffa3adf
erekle khomasuridze <ereklekhomasuridze@gmail.com>
2024-07-10 13:44:25 +0400

    Started testing concurrency in .NET

---
+++ WorkingWithTasks/WorkingWithTasks/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using static System.Console;
+
+namespace WorkingWithTasks;
+
+partial class Program
+{
+    static void Main(string[] args)
+    {
+        OutputThreadInfo();
+        Stopwatch timer = Stopwatch.StartNew();
+        SectionTitle("Running methods synchronously on one thread.");
+        MethodA();
+        MethodB();
+        MethodC();
+        WriteLine($"{timer.ElapsedMilliseconds:#,##0}ms elapsed.");
+    }
+}
