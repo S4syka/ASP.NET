b1dbd68548ede9deaf8a12285e236a6d1b909d48
erekle khomasuridze <ereklekhomasuridze@gmail.com>
2024-07-10 16:22:21 +0400

    Tested EnumarableAsync

---
+++ WorkingWithTasks/AsyncEnumerable/Program.Methods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncEnumerable;
+
+internal partial class Program
+{
+    private static async IAsyncEnumerable<int> GetNumbersAsync()
+    {
+        Random r = new Random();
+        
+        await Task.Delay(r.Next(1500, 3000));
+        yield return r.Next(1, 1001);
+
+        await Task.Delay(r.Next(1500, 3000));
+        yield return r.Next(1, 1001);
+        
+        await Task.Delay(r.Next(1500, 3000));
+        yield return r.Next(1, 1001);
+    }
+}
